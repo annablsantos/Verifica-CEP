025c8289f851f49b186a5c23314da40c01ad5fa4
Anna Beatriz <109621978+annablsantos@users.noreply.github.com>
2023-09-24 23:38:13 -0300

    Adicionando segundo cenário de teste

--- src/test/java/VerificaCepTest.java
+++ src/test/java/VerificaCepTest.java
@@ -1,22 +1,23 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VerificaCepTest {
     private String BASE_URL = "https://viacep.com.br/ws/";
-
     @BeforeEach
-    void init(){
+    void init() {
         RestAssured.baseURI = BASE_URL;
     }
 
     @Test
-    @DisplayName("Deve verificar se o CEP passado na URL é válido, validando o Response Body de acordo com o tal schema JSON")
+    @DisplayName("Deve verificar se o CEP inserido na URL é válido, validando o Response Body de acordo com o tal schema JSON.")
     void testeVerificandoUmCepValido() {
         given()
                 .accept(ContentType.JSON)
@@ -27,7 +28,22 @@ public class VerificaCepTest {
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("schema/SchemaCep.json"));
     }
-}
 
+    @Test
+    @DisplayName("Deve verificar se o CEP inserido na URL é inválido.")
+    void testeVerificandoUmCepInvalido() {
+        int statusCode =
+                given()
+                        .accept(ContentType.JSON)
+                        .pathParam("cep", "aeiou123")
+                .when()
+                        .get("/{cep}/json")
+                .then()
+                        .extract()
+                        .statusCode();
+
+        assertEquals(400, statusCode);
+    }
+}
 
 
